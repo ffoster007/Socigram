51f99e15638e47e556b47ee4d5006bb954b4105a
FZOEkn <penhnungcaphuli@gmail.com>
2025-10-06 00:09:27 +0700

    Add_Analytics_Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React from "react";
 import Sociogram from "./components/Sociogram";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Page() {
   return (
+    <>
+    <Analytics />
     <main className="p-6">
       <Sociogram />
     </main>
+    </>
   );
 }
